0e2cb195d3341ebbf6208df33e273c42fa2e83ce
Iker Arana <ikera1123@hotmail.com>
2025-07-25 23:46:57 +0200

    Initial project push

---
+++ scheduletron3000/README.md
@@ -0,0 +1,11 @@
+# ScheduleTron 3000
+
+<p align="center">
+  <a href="https://philippspiess.com/scheduling-in-react/">
+    <img alt="ScheduleTron 3000" src="http://cl.ly/6ba38f62fc73/Scheduling%20in%20React.gif" width="720">
+  </a>
+</p>
+
+An example app used to demonstrate the new React features Concurrent React and the Scheduler.
+
+[Read more in this blog post about Scheduling in React.](https://philippspiess.com/scheduling-in-react/)
---
+++ scheduletron3000/src/index.js
@@ -0,0 +1,102 @@
+import { NameList, sendAnalyticsPing } from "./helpers";
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+import Description from "./Description";
+
+import {
+  unstable_LowPriority,
+  unstable_next,
+  unstable_runWithPriority,
+  unstable_scheduleCallback
+} from "scheduler";
+
+import "./styles.css";
+
+// Change the flag below to enable Concurrent React and Scheduler improvements
+// as layed out in https://philippspiess.com/scheduling-in-react
+export const CONCURRENT_AND_SCHEDULED = true;
+
+class App extends React.Component {
+  state = {
+    searchValue: ""
+  };
+
+  handleChange = value => {
+    this.setState({ searchValue: value });
+  };
+
+  render() {
+    const { searchValue } = this.state;
+
+    return (
+      <div className="App">
+        <Header>ScheduleTron 3000</Header>
+        <SearchBox onChange={this.handleChange} />
+        <NameList searchValue={searchValue} />
+        <Description />
+      </div>
+    );
+  }
+}
+
+class SearchBox extends React.Component {
+  state = {
+    inputValue: ""
+  };
+
+  handleChange = event => {
+    const value = event.target.value;
+    const onChange = this.props.onChange;
+
+    if (CONCURRENT_AND_SCHEDULED) {
+      this.setState({ inputValue: value });
+
+      unstable_next(function() {
+        onChange(value);
+      });
+
+      sendDeferredAnalyticsPing(value);
+    } else {
+      this.setState({ inputValue: value });
+      onChange(value);
+      sendAnalyticsPing(value);
+    }
+  };
+
+  render() {
+    const { inputValue } = this.state;
+
+    return (
+      <div className="clearfix">
+        <input
+          type="text"
+          className="input"
+          value={inputValue}
+          onChange={this.handleChange}
+          placeholder="Filter 🔍"
+        />
+      </div>
+    );
+  }
+}
+
+function sendDeferredAnalyticsPing(value) {
+  unstable_runWithPriority(unstable_LowPriority, function() {
+    unstable_scheduleCallback(function() {
+      sendAnalyticsPing(value);
+    });
+  });
+}
+
+const rootElement = document.getElementById("root");
+const Wrapper = ReactDOM.render(
+  CONCURRENT_AND_SCHEDULED ? (
+    <React.unstable_ConcurrentMode>
+      <App />
+    </React.unstable_ConcurrentMode>
+  ) : (
+    <App />
+  ),
+  rootElement
+);
